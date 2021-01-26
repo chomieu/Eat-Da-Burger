0bacc6bb7db7f08bcf7eb296c20b1c4b3afd3644
chomieu <chomieu@gmail.com>
2021-01-25 23:55:37 -0800

    Added process.env.JAWSDB to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+let connection
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgerDB" // Changed database name
-});
+process.env.JAWSDB_URL ? connection = mysql.createConnection(process.env.JAWSDB_URL) :
+  mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgerDB" // Changed database name
+  });
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
