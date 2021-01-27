6295d9a9a3c4e55eefa308377e3db7da7ee88550
chomieu <chomieu@gmail.com>
2021-01-26 20:28:08 -0800

    Edited comments in orm.js

--- config/orm.js
+++ config/orm.js
@@ -7,23 +7,17 @@ function objToSql(ob) {
   // loop through the keys and push the key/value as a string int arr
   for (var key in ob) {
     var value = ob[key];
-    // check to skip hidden properties
     if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {sleepy: true} => ["sleepy=true"]
       arr.push(key + "=" + value);
     }
   }
-
-  // translate array of strings to a single comma-separated string
   return arr.toString();
 }
 
-// Object for all our SQL statement functions.
+// Object for all SQL statement functions
 var orm = {
   all: function(table, cb) {
     connection.query(`SELECT * FROM ${table}`, (err, result) => {
@@ -51,5 +45,5 @@ var orm = {
   }
 }
 
-// Export the orm object for the model (burger.js).
+// Export the orm object for the model (burger.js)
 module.exports = orm
